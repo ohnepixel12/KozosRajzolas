b7d51cd65bf4ea3c26c55ed139981878ac0069b8
KrucsayBogdán(IRU_9e <bacongeneral01@gmail.com>
2026-05-13 10:28:32 +0200

    törzs

--- KozosRajz.py
+++ KozosRajz.py
@@ -2,12 +2,18 @@ import turtle
 
 i=0
 turtle.penup()
+turtle.setposition(0, -100)
+turtle.fillcolor("brown")
+turtle.pencolor("brown")
+
 while i<2:
-    turtle.setposition(0, -100)
+    turtle.begin_fill()
     turtle.pendown()
-    turtle.forward(20)
-    i +=1
-
-
+    turtle.forward(40)
+    turtle.left(90)
+    turtle.forward(150)
+    turtle.left(90)
+    turtle.end_fill()
+    i+=1
 
 turtle.done()
